a9baa4fc8c8f83bdee2055edd4e71801432b4494
Ravikumarsharmaofficial <68783112+Ravikumarsharmaofficial@users.noreply.github.com>
2023-04-22 01:29:45 +0530

    multi select while adding employee added

--- src/screens/Employee/AddEmployee/AddEmployee.tsx
+++ src/screens/Employee/AddEmployee/AddEmployee.tsx
@@ -1,11 +1,34 @@
 import { Box, Button, Grid, Paper, Typography } from "@mui/material";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { makeStyles, createStyles, Theme, useTheme } from "@material-ui/core/styles";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { UrlConstants } from "../../../global/UrlConstants";
+import { Chip, InputLabel, MenuItem, OutlinedInput } from "@material-ui/core";
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+function getStyles(name: string, personName: readonly string[], theme: Theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -34,10 +57,12 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function AddEmployee(props: any) {
   const classes = useStyles();
+  const theme = useTheme();
   const history = useHistory();
   const [citiOptions, setCitiOptions] = useState([]);
   const [zoneOptions, setZoneOptions] = useState([]);
   const [edit, setEdit] = useState(props.history.location.state?.data);
+  const [personName, setPersonName] = React.useState<string[]>(edit ? JSON.parse(edit.city) : []);
   const [employeeData, setEmployeeData] = useState({
     id: edit?.id ?? "",
     name: edit?.name ?? "",
@@ -193,6 +218,21 @@ export default function AddEmployee(props: any) {
     });
   };
 
+  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    console.log("jghgg", JSON.stringify(event.target.value))
+    setEmployeeData({
+      ...employeeData,
+      [event.target.name]: JSON.stringify(event.target.value),
+    });
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
   return (
     <div
       style={{
@@ -244,7 +284,7 @@ export default function AddEmployee(props: any) {
             </Box>
           </Grid>
           <Grid item xs>
-            <Typography className={classes.Typography}>* City/Town</Typography>
+            {/* <Typography className={classes.Typography}>* City/Town</Typography>
             <select
               className={classes.select}
               name="city"
@@ -259,8 +299,47 @@ export default function AddEmployee(props: any) {
                   {x}
                 </option>
               ))}
-            </select>
+            </select> */}
+            <Typography className={classes.Typography}>
+              * City / Town
+            </Typography>
+            <Box>
+              <FormControl sx={{ m: 1, width: 300 }}>
+                <Select
+                  labelId="demo-multiple-chip-label"
+                  id="demo-multiple-chip"
+                  multiple
+                  name="city"
+                  size="small"
+                  label="City"
+                  // value={[...employeeData.city]}
+                  value={personName}
+                  // onChange={handleInputChange}
+                  onChange={handleChange}
+                  input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+                  renderValue={(selected: any) => (
+                    <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                      {selected.map((value: any) => (
+                        <Chip key={value} label={value} />
+                      ))}
+                    </Box>
+                  )}
+                  MenuProps={MenuProps}
+                >
+                  {citiOptions.map((name) => (
+                    <MenuItem
+                      key={name}
+                      value={name}
+                      style={getStyles(name, personName, theme)}
+                    >
+                      {name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
           </Grid>
+
           {!edit && (
             <Grid className={classes.input} item xs>
               <Typography className={classes.Typography}>* Password</Typography>
