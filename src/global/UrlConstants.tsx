1cf7bacff243377be81653af4ac50777a6c5c3aa
Ravikumarsharmaofficial <68783112+Ravikumarsharmaofficial@users.noreply.github.com>
2023-04-23 02:45:53 +0530

    url constant fixed

--- src/global/UrlConstants.tsx
+++ src/global/UrlConstants.tsx
@@ -1,6 +1,6 @@
 export const UrlConstants = {
   // baseUrl: `https://backendapi.axisinfoline.com`,
   // baseUrl: `http://localhost:8080`,
-  baseUrl: `http://64.227.150.107:9000,
+  baseUrl: `http://64.227.150.107:9000',
   // baseUrl: `http://localhost:9000`,
 };
